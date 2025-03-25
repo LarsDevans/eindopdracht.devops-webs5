3f0eb8b0749280e02a0d7499f5474c1fbe111f29
Lars Devans <l.beijaard1@student.avans.nl>
2025-03-26 00:13:47 +0100

    Add initial TypeORM import for a database connection

--- apps/clock-service/src/clock-service.module.ts
+++ apps/clock-service/src/clock-service.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ClockServiceController } from './clock-service.controller';
 import { ClockServiceService } from './clock-service.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql-service',
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_ROOT_USER,
+      password: process.env.MYSQL_ROOT_PASSWORD,
+      database: process.env.MYSQL_CLOCK_DB,
+    }),
+  ],
   controllers: [ClockServiceController],
   providers: [ClockServiceService],
 })
--- apps/submission-service/src/submission-service.module.ts
+++ apps/submission-service/src/submission-service.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { SubmissionServiceController } from './submission-service.controller';
 import { SubmissionServiceService } from './submission-service.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql-service',
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_ROOT_USER,
+      password: process.env.MYSQL_ROOT_PASSWORD,
+      database: process.env.MYSQL_SUBMISSION_DB,
+    }),
+  ],
   controllers: [SubmissionServiceController],
   providers: [SubmissionServiceService],
 })
