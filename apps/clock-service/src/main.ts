d34a01f01e5e5294a4bc5118d4723848bc46597a
Lars Devans <l.beijaard1@student.avans.nl>
2025-04-08 15:10:39 +0200

    Update RabbitMQ queue name configs (#22)
    * Update RabbitMQ queue name configs
    
    * Add queue event example

--- apps/clock-service/src/main.ts
+++ apps/clock-service/src/main.ts
@@ -2,24 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { ClockServiceModule } from './clock-service.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app =
     await NestFactory.create<NestExpressApplication>(ClockServiceModule);
-
-  const queues = [process.env.RABBITMQ_TARGET_COMPLETED_QUEUE];
-
-  queues.forEach((queue) => {
-    app.connectMicroservice({
-      transport: Transport.RMQ,
-      options: {
-        urls: [`${process.env.RABBITMQ_URI}:${process.env.RABBITMQ_PORT}`],
-        queue,
-        noAck: false,
-      },
-    });
-  });
 
   // Swagger / OA docs
   const config = new DocumentBuilder()
--- apps/target-service/src/main.ts
+++ apps/target-service/src/main.ts
@@ -2,27 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { TargetServiceModule } from './target-service.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app =
     await NestFactory.create<NestExpressApplication>(TargetServiceModule);
-
-  const queues = [
-    process.env.RABBITMQ_TARGET_CREATED_QUEUE,
-    process.env.RABBITMQ_TARGET_REVOKED_QUEUE,
-  ];
-
-  queues.forEach((queue) => {
-    app.connectMicroservice({
-      transport: Transport.RMQ,
-      options: {
-        urls: [`${process.env.RABBITMQ_URI}:${process.env.RABBITMQ_PORT}`],
-        queue,
-        noAck: false,
-      },
-    });
-  });
 
   // Swagger / OA docs
   const config = new DocumentBuilder()
