eba02ac6ad827c3e73129074a97a1274fcb30c7d
Lars Devans <l.beijaard1@student.avans.nl>
2025-04-11 00:02:03 +0200

    Remove some resource boilerplate

--- apps/mail-service/src/users/users.controller.ts
+++ apps/mail-service/src/users/users.controller.ts
@@ -2,7 +2,6 @@ import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller()
 export class UsersController {
@@ -11,25 +10,5 @@ export class UsersController {
   @MessagePattern('createUser')
   create(@Payload() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
-  }
-
-  @MessagePattern('findAllUsers')
-  findAll() {
-    return this.usersService.findAll();
-  }
-
-  @MessagePattern('findOneUser')
-  findOne(@Payload() id: number) {
-    return this.usersService.findOne(id);
-  }
-
-  @MessagePattern('updateUser')
-  update(@Payload() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(updateUserDto.id, updateUserDto);
-  }
-
-  @MessagePattern('removeUser')
-  remove(@Payload() id: number) {
-    return this.usersService.remove(id);
   }
 }
