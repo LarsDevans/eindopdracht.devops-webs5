a8de52cb00c1db137bb5cc417da542f7fb5a6721
Lars Devans <l.beijaard1@student.avans.nl>
2025-04-11 15:06:35 +0200

    Update payload datatype

--- libs/kafka/src/kafka.service.ts
+++ libs/kafka/src/kafka.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
 
 import { KAFKA_CLIENT_NAME } from './kafka.constants';
+import { TopicPayload } from '@app/types';
 
 @Injectable()
 export class KafkaService implements OnModuleInit {
@@ -14,7 +15,7 @@ export class KafkaService implements OnModuleInit {
     await this.kafkaClient.connect();
   }
 
-  emit(topic: string, payload: any) {
+  emit(topic: string, payload: TopicPayload) {
     return this.kafkaClient.emit(topic, payload);
   }
 }
